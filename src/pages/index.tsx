d04123d95f27d1ada375a1110348a3532cb0f602
Bruno Guirra <br.guirra@gmail.com>
2021-07-10 16:27:42 -0300

    feat: create custom document and import fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,9 +24,9 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return <div>Hellow World!</div>;
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
